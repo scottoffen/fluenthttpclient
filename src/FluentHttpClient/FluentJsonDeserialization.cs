b1a9161bf3d1101b52b884978dad24e3f4621521
Scott Offen <3513626+scottoffen@users.noreply.github.com>
2024-02-14 17:14:52 -0700

    Enable nullable

--- src/FluentHttpClient/FluentJsonDeserialization.cs
+++ src/FluentHttpClient/FluentJsonDeserialization.cs
@@ -4,43 +4,43 @@ namespace FluentHttpClient;
 
 public static class FluentJsonDeserialization
 {
-    public static async Task<T> DeserializeJsonAsync<T>(this Task<Stream> result)
+    public static async Task<T?> DeserializeJsonAsync<T>(this Task<Stream> result)
     {
         return await result.DeserializeJsonAsync<T>(FluentHttpClient.DefaultJsonSerializerOptions);
     }
 
-    public static async Task<T> DeserializeJsonAsync<T>(this Task<Stream> result, JsonSerializerOptions options)
+    public static async Task<T?> DeserializeJsonAsync<T>(this Task<Stream> result, JsonSerializerOptions options)
     {
         return await JsonSerializer.DeserializeAsync<T>(await result, options);
     }
 
-    public static async Task<T> DeserializeJsonAsync<T>(this Task<String> result)
+    public static async Task<T?> DeserializeJsonAsync<T>(this Task<String> result)
     {
         return await result.DeserializeJsonAsync<T>(FluentHttpClient.DefaultJsonSerializerOptions);
     }
 
-    public static async Task<T> DeserializeJsonAsync<T>(this Task<string> result, JsonSerializerOptions options)
+    public static async Task<T?> DeserializeJsonAsync<T>(this Task<string> result, JsonSerializerOptions options)
     {
         return JsonSerializer.Deserialize<T>(await result, options);
     }
 
-    public static async Task<T> DeserializeJsonAsync<T>(this Task<HttpResponseMessage> result)
+    public static async Task<T?> DeserializeJsonAsync<T>(this Task<HttpResponseMessage> result)
     {
         return await result.DeserializeJsonAsync<T>(FluentHttpClient.DefaultJsonSerializerOptions);
     }
 
-    public static async Task<T> DeserializeJsonAsync<T>(this Task<HttpResponseMessage> taskResponse, JsonSerializerOptions options)
+    public static async Task<T?> DeserializeJsonAsync<T>(this Task<HttpResponseMessage> taskResponse, JsonSerializerOptions options)
     {
         var response = await taskResponse;
         return JsonSerializer.Deserialize<T>(await response.GetResponseStreamAsync(), options);
     }
 
-    public static async Task<T> DeserializeJsonAsync<T>(this Task<HttpResponseMessage> result, Func<HttpResponseMessage, T> defaultAction)
+    public static async Task<T?> DeserializeJsonAsync<T>(this Task<HttpResponseMessage> result, Func<HttpResponseMessage, T> defaultAction)
     {
         return await result.DeserializeJsonAsync<T>(defaultAction, FluentHttpClient.DefaultJsonSerializerOptions);
     }
 
-    public static async Task<T> DeserializeJsonAsync<T>(this Task<HttpResponseMessage> taskResponse, Func<HttpResponseMessage, T> defaultAction, JsonSerializerOptions options)
+    public static async Task<T?> DeserializeJsonAsync<T>(this Task<HttpResponseMessage> taskResponse, Func<HttpResponseMessage, T> defaultAction, JsonSerializerOptions options)
     {
         var response = await taskResponse;
         return (response.IsSuccessStatusCode)
@@ -48,12 +48,12 @@ public static class FluentJsonDeserialization
             : defaultAction(response);
     }
 
-    public static async Task<T> DeserializeJsonAsync<T>(this Task<HttpResponseMessage> result, Func<HttpResponseMessage, Task<T>> defaultAction)
+    public static async Task<T?> DeserializeJsonAsync<T>(this Task<HttpResponseMessage> result, Func<HttpResponseMessage, Task<T>> defaultAction)
     {
         return await result.DeserializeJsonAsync<T>(defaultAction, FluentHttpClient.DefaultJsonSerializerOptions);
     }
 
-    public static async Task<T> DeserializeJsonAsync<T>(this Task<HttpResponseMessage> taskResponse, Func<HttpResponseMessage, Task<T>> defaultAction, JsonSerializerOptions options)
+    public static async Task<T?> DeserializeJsonAsync<T>(this Task<HttpResponseMessage> taskResponse, Func<HttpResponseMessage, Task<T>> defaultAction, JsonSerializerOptions options)
     {
         var response = await taskResponse;
         return (response.IsSuccessStatusCode)
