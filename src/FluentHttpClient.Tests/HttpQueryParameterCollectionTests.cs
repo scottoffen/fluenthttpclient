0c13e371e65ac7e1ec3472fef4760d6c72605604
Scott Offen <3513626+scottoffen@users.noreply.github.com>
2025-11-14 19:40:43 -0700

    Add HttpQueryParameterCollection and unit tests

---
+++ src/FluentHttpClient.Tests/HttpQueryParameterCollectionTests.cs
@@ -0,0 +1,323 @@
+﻿namespace FluentHttpClient.Tests;
+
+public class HttpQueryParameterCollectionTests
+{
+    [Fact]
+    public void IsEmpty_ReturnsTrue_WhenCollectionIsNew()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        collection.IsEmpty.ShouldBeTrue();
+        collection.Count.ShouldBe(0);
+    }
+
+    [Fact]
+    public void Add_AddsSingleValue_WhenKeyIsValid()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        collection.Add("key", "value");
+
+        collection.IsEmpty.ShouldBeFalse();
+        collection.Count.ShouldBe(1);
+        collection["key"].ShouldBe(new[] { "value" });
+    }
+
+    [Fact]
+    public void Add_AddsMultipleValues_WhenCalledMultipleTimesForSameKey()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        collection.Add("key", "value1");
+        collection.Add("key", "value2");
+
+        collection.Count.ShouldBe(1);
+        collection["key"].ShouldBe(new[] { "value1", "value2" });
+    }
+
+    [Fact]
+    public void Add_AddsMultipleValues_WhenEnumerableOverloadUsed()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        collection.AddRange("key", new[] { "value1", "value2" });
+
+        collection.Count.ShouldBe(1);
+        collection["key"].ShouldBe(new[] { "value1", "value2" });
+    }
+
+    [Fact]
+    public void Set_ReplacesExistingValues_WhenKeyAlreadyExists()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        collection.Add("key", "value1");
+        collection.Set("key", "value2");
+
+        collection.Count.ShouldBe(1);
+        collection["key"].ShouldBe(new[] { "value2" });
+    }
+
+    [Fact]
+    public void Set_ReplacesExistingValues_WhenEnumerableOverloadUsed()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        collection.Add("key", "value1");
+        collection.SetRange("key", new[] { "value2", "value3" });
+
+        collection.Count.ShouldBe(1);
+        collection["key"].ShouldBe(new[] { "value2", "value3" });
+    }
+
+    [Fact]
+    public void Remove_RemovesKeyAndReturnsTrue_WhenKeyExists()
+    {
+        var collection = new HttpQueryParameterCollection();
+        collection.Add("key", "value");
+
+        var result = collection.Remove("key");
+
+        result.ShouldBeTrue();
+        collection.ContainsKey("key").ShouldBeFalse();
+        collection.Count.ShouldBe(0);
+    }
+
+    [Fact]
+    public void Remove_ReturnsFalse_WhenKeyDoesNotExist()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        var result = collection.Remove("missing");
+
+        result.ShouldBeFalse();
+    }
+
+    [Fact]
+    public void Clear_RemovesAllParameters_WhenCalled()
+    {
+        var collection = new HttpQueryParameterCollection();
+        collection.Add("key1", "value1");
+        collection.Add("key2", "value2");
+
+        collection.Clear();
+
+        collection.Count.ShouldBe(0);
+        collection.IsEmpty.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void ContainsKey_ReturnsTrue_WhenKeyExists()
+    {
+        var collection = new HttpQueryParameterCollection();
+        collection.Add("key", "value");
+
+        collection.ContainsKey("key").ShouldBeTrue();
+    }
+
+    [Fact]
+    public void ContainsKey_ReturnsFalse_WhenKeyDoesNotExist()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        collection.ContainsKey("key").ShouldBeFalse();
+    }
+
+    [Fact]
+    public void TryGetValues_ReturnsTrueAndValues_WhenKeyExists()
+    {
+        var collection = new HttpQueryParameterCollection();
+        collection.AddRange("key", new[] { "value1", "value2" });
+
+        var result = collection.TryGetValues("key", out var values);
+
+        result.ShouldBeTrue();
+        values.ShouldBe(new[] { "value1", "value2" });
+    }
+
+    [Fact]
+    public void TryGetValues_ReturnsFalseAndEmptyList_WhenKeyDoesNotExist()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        var result = collection.TryGetValues("missing", out var values);
+
+        result.ShouldBeFalse();
+        values.ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void Indexer_ReturnsValues_WhenKeyExists()
+    {
+        var collection = new HttpQueryParameterCollection();
+        collection.Add("key", "value");
+
+        var values = collection["key"];
+
+        values.ShouldBe(new[] { "value" });
+    }
+
+    [Fact]
+    public void Indexer_ThrowsKeyNotFound_WhenKeyDoesNotExist()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        Should.Throw<KeyNotFoundException>(() =>
+        {
+            var _ = collection["missing"];
+        });
+    }
+
+    [Fact]
+    public void Add_ThrowsArgumentNullException_WhenKeyIsNull()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        Should.Throw<ArgumentNullException>(() => collection.Add(null!, "value"));
+    }
+
+    [Fact]
+    public void Add_ThrowsArgumentException_WhenKeyIsWhitespace()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        Should.Throw<ArgumentException>(() => collection.Add("   ", "value"));
+    }
+
+    [Fact]
+    public void AddEnumerable_ThrowsArgumentNullException_WhenValuesIsNull()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        Should.Throw<ArgumentNullException>(() => collection.AddRange("key", (IEnumerable<string?>)null!));
+    }
+
+    [Fact]
+    public void SetEnumerable_ThrowsArgumentNullException_WhenValuesIsNull()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        Should.Throw<ArgumentNullException>(() => collection.SetRange("key", (IEnumerable<string?>)null!));
+    }
+
+    [Fact]
+    public void Remove_ThrowsArgumentNullException_WhenKeyIsNull()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        Should.Throw<ArgumentNullException>(() => collection.Remove(null!));
+    }
+
+    [Fact]
+    public void ContainsKey_ThrowsArgumentNullException_WhenKeyIsNull()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        Should.Throw<ArgumentNullException>(() => collection.ContainsKey(null!));
+    }
+
+    [Fact]
+    public void TryGetValues_ThrowsArgumentNullException_WhenKeyIsNull()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        Should.Throw<ArgumentNullException>(() =>
+        {
+            collection.TryGetValues(null!, out _);
+        });
+    }
+
+    [Fact]
+    public void ToQueryString_ReturnsEmptyString_WhenCollectionIsEmpty()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        var query = collection.ToQueryString();
+
+        query.ShouldBe(string.Empty);
+    }
+
+    [Fact]
+    public void ToQueryString_RendersSingleKeyValuePair_WhenOneParameter()
+    {
+        var collection = new HttpQueryParameterCollection();
+        collection.Add("key", "value");
+
+        var query = collection.ToQueryString();
+
+        query.ShouldBe("?key=value");
+    }
+
+    [Fact]
+    public void ToQueryString_RendersMultipleParameters_WhenMultipleKeysAndValues()
+    {
+        var collection = new HttpQueryParameterCollection();
+        collection.Add("a", "1");
+        collection.Add("b", "2");
+
+        var query = collection.ToQueryString();
+
+        query.ShouldBe("?a=1&b=2");
+    }
+
+    [Fact]
+    public void ToQueryString_RendersMultipleValuesForSameKey_WhenMultipleValuesPresent()
+    {
+        var collection = new HttpQueryParameterCollection();
+        collection.Add("key", "value1");
+        collection.Add("key", "value2");
+
+        var query = collection.ToQueryString();
+
+        query.ShouldBe("?key=value1&key=value2");
+    }
+
+    [Fact]
+    public void ToQueryString_EncodesKeysAndValues_WhenTheyContainSpecialCharacters()
+    {
+        var collection = new HttpQueryParameterCollection();
+        collection.Add("sp ce", "a+b&c=d");
+
+        var query = collection.ToQueryString();
+
+        query.ShouldBe("?sp%20ce=a%2Bb%26c%3Dd");
+    }
+
+    [Fact]
+    public void ToQueryString_RendersFlagParameter_WhenValueIsNull()
+    {
+        var collection = new HttpQueryParameterCollection();
+        collection.Add("flag", null);
+
+        var query = collection.ToQueryString();
+
+        query.ShouldBe("?flag");
+    }
+
+    [Fact]
+    public void ToQueryString_RendersFlagParameter_WhenNoValuesPresent()
+    {
+        var collection = new HttpQueryParameterCollection();
+
+        collection.SetRange("empty", Array.Empty<string?>());
+
+        var query = collection.ToQueryString();
+
+        query.ShouldBe("?empty");
+    }
+
+    [Fact]
+    public void GetEnumerator_EnumeratesAllParameters_WhenCollectionIsNotEmpty()
+    {
+        var collection = new HttpQueryParameterCollection();
+        collection.Add("a", "1");
+        collection.Add("b", "2");
+        collection.Add("b", "3");
+
+        var entries = collection.ToList();
+
+        entries.Count.ShouldBe(2);
+        entries.Single(kvp => kvp.Key == "a").Value.ShouldBe(new[] { "1" });
+        entries.Single(kvp => kvp.Key == "b").Value.ShouldBe(new[] { "2", "3" });
+    }
+}
---
+++ src/FluentHttpClient/HttpQueryParameterCollection.cs
@@ -0,0 +1,284 @@
+using System.Collections;
+using System.Text;
+
+namespace FluentHttpClient;
+
+/// <summary>
+/// Represents a collection of HTTP query string parameters that supports multiple values per key.
+/// </summary>
+/// <remarks>
+/// All query string values for <see cref="HttpRequestBuilder"/> are composed from this collection.
+/// Keys are case sensitive. Values are not automatically converted from other types.
+/// </remarks>
+public sealed class HttpQueryParameterCollection :
+    IEnumerable<KeyValuePair<string, IReadOnlyList<string?>>>
+{
+    private readonly Dictionary<string, List<string?>> _parameters =
+        new(StringComparer.Ordinal);
+
+    /// <summary>
+    /// Gets the number of distinct parameter keys in the collection.
+    /// </summary>
+    public int Count => _parameters.Count;
+
+    /// <summary>
+    /// Gets a value that indicates whether the collection is empty.
+    /// </summary>
+    public bool IsEmpty => _parameters.Count == 0;
+
+    /// <summary>
+    /// Gets the values associated with the specified key.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>
+    /// A read-only list of values for the key.
+    /// </returns>
+    /// <exception cref="KeyNotFoundException">
+    /// Thrown when the key does not exist in the collection.
+    /// </exception>
+    public IReadOnlyList<string?> this[string key] => _parameters[key];
+
+    /// <summary>
+    /// Adds a parameter value for the specified key.
+    /// </summary>
+    /// <param name="key">The parameter name.</param>
+    /// <param name="value">
+    /// The parameter value. If null, the parameter is rendered as a flag
+    /// (for example, <c>?key</c>).
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="key"/> is null.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// Thrown when <paramref name="key"/> is empty or consists only of white space.
+    /// </exception>
+    public void Add(string key, string? value)
+    {
+        ValidateKey(key);
+
+        if (!_parameters.TryGetValue(key, out var list))
+        {
+            list = new List<string?>();
+            _parameters[key] = list;
+        }
+
+        list.Add(value);
+    }
+
+    /// <summary>
+    /// Adds one or more values for the specified key.
+    /// </summary>
+    /// <param name="key">The parameter name.</param>
+    /// <param name="values">The parameter values.</param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="key"/> or <paramref name="values"/> is null.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// Thrown when <paramref name="key"/> is empty or consists only of white space.
+    /// </exception>
+    public void AddRange(string key, IEnumerable<string?> values)
+    {
+        ArgumentNullException.ThrowIfNull(values);
+
+        ValidateKey(key);
+
+        if (!_parameters.TryGetValue(key, out var list))
+        {
+            list = new List<string?>();
+            _parameters[key] = list;
+        }
+
+        foreach (var value in values)
+        {
+            list.Add(value);
+        }
+    }
+
+    /// <summary>
+    /// Replaces any existing values for the specified key with a single value.
+    /// </summary>
+    /// <param name="key">The parameter name.</param>
+    /// <param name="value">
+    /// The parameter value. If null, the parameter is rendered as a flag
+    /// (for example, <c>?key</c>).
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="key"/> is null.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// Thrown when <paramref name="key"/> is empty or consists only of white space.
+    /// </exception>
+    public void Set(string key, string? value)
+    {
+        ValidateKey(key);
+
+        _parameters[key] = new List<string?> { value };
+    }
+
+    /// <summary>
+    /// Replaces any existing values for the specified key with the provided values.
+    /// </summary>
+    /// <param name="key">The parameter name.</param>
+    /// <param name="values">The parameter values.</param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="key"/> or <paramref name="values"/> is null.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// Thrown when <paramref name="key"/> is empty or consists only of white space.
+    /// </exception>
+    public void SetRange(string key, IEnumerable<string?> values)
+    {
+        ArgumentNullException.ThrowIfNull(values);
+
+        ValidateKey(key);
+
+        _parameters[key] = new List<string?>(values);
+    }
+
+    /// <summary>
+    /// Removes all values for the specified key.
+    /// </summary>
+    /// <param name="key">The parameter name.</param>
+    /// <returns>
+    /// true if the key was found and removed; otherwise false.
+    /// </returns>
+    public bool Remove(string key)
+    {
+        ArgumentNullException.ThrowIfNull(key);
+
+        return _parameters.Remove(key);
+    }
+
+    /// <summary>
+    /// Removes all parameters from the collection.
+    /// </summary>
+    public void Clear()
+    {
+        _parameters.Clear();
+    }
+
+    /// <summary>
+    /// Determines whether the collection contains the specified key.
+    /// </summary>
+    /// <param name="key">The parameter name.</param>
+    /// <returns>
+    /// true if the key exists in the collection; otherwise false.
+    /// </returns>
+    public bool ContainsKey(string key)
+    {
+        ArgumentNullException.ThrowIfNull(key);
+
+        return _parameters.ContainsKey(key);
+    }
+
+    /// <summary>
+    /// Attempts to get the values associated with the specified key.
+    /// </summary>
+    /// <param name="key">The parameter name.</param>
+    /// <param name="values">
+    /// When this method returns, contains the values associated with the key,
+    /// or an empty list if the key is not found.
+    /// </param>
+    /// <returns>
+    /// true if the key exists in the collection; otherwise false.
+    /// </returns>
+    public bool TryGetValues(string key, out IReadOnlyList<string?> values)
+    {
+        ArgumentNullException.ThrowIfNull(key);
+
+        if (_parameters.TryGetValue(key, out var list))
+        {
+            values = list;
+            return true;
+        }
+
+        values = [];
+        return false;
+    }
+
+    /// <summary>
+    /// Returns a query string representation of the collection.
+    /// </summary>
+    /// <returns>
+    /// A query string starting with '?', or an empty string if the collection is empty.
+    /// </returns>
+    /// <remarks>
+    /// Keys and values are encoded using <see cref="Uri.EscapeDataString(string)"/>.
+    /// Parameters with null values are rendered as flags (for example, <c>?key</c>).
+    /// Keys with no values are omitted.
+    /// </remarks>
+    public string ToQueryString()
+    {
+        if (_parameters.Count == 0)
+        {
+            return string.Empty;
+        }
+
+        var builder = new StringBuilder();
+        var first = true;
+
+        void AppendPrefix()
+        {
+            if (first)
+            {
+                builder.Append('?');
+                first = false;
+            }
+            else
+            {
+                builder.Append('&');
+            }
+        }
+
+        foreach (var pair in _parameters)
+        {
+            var key = pair.Key;
+            var values = pair.Value;
+            var encodedName = Uri.EscapeDataString(key);
+
+            if (values.Count == 0)
+            {
+                // No values → render as flag: ?key
+                AppendPrefix();
+                builder.Append(encodedName);
+                continue;
+            }
+
+            foreach (var value in values)
+            {
+                AppendPrefix();
+                builder.Append(encodedName);
+
+                if (value is not null)
+                {
+                    builder.Append('=');
+                    builder.Append(Uri.EscapeDataString(value));
+                }
+            }
+        }
+
+        return builder.ToString();
+    }
+
+    /// <inheritdoc />
+    public IEnumerator<KeyValuePair<string, IReadOnlyList<string?>>> GetEnumerator()
+    {
+        foreach (var pair in _parameters)
+        {
+            yield return new KeyValuePair<string, IReadOnlyList<string?>>(pair.Key, pair.Value);
+        }
+    }
+
+    /// <inheritdoc />
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private static void ValidateKey(string key)
+    {
+        ArgumentNullException.ThrowIfNull(key);
+
+        if (string.IsNullOrWhiteSpace(key))
+        {
+            throw new ArgumentException("Key cannot be empty or consist only of white space.", nameof(key));
+        }
+    }
+}
